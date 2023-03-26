1f3fb906d762888e7d2c203d96ded01d811ca454
Sujal Kumar Sinha <sujalksinha23@gmail.com>
2023-03-26 21:12:51 +0530

    conditional statements

---
+++ conditional_statements.dart
@@ -0,0 +1,17 @@
+//if , else if and else are conditional statements
+
+/*syntax for these are similar to syntax function where you
+  put fname/if (parameter/condition){ body}*/
+
+void main() {
+  int x = 7;
+  const y = 9;
+
+  if (x == y) {
+    print("Both are equal");
+  } else if (x > y) {
+    print("x is greater than  y");
+  } else {
+    print("y is greater than x");
+  }
+}
--- data_types.dart
+++ data_types.dart
@@ -1,5 +1,14 @@
 //dart has many types of data
 
 main() {
-  print("Hello");
+  const num = 45;
+  //const means the value of variable is constant for the entire program time you cant initate a variable and assign value to it later it must be defined at the time of initializing only
+
+  //final means once a value is assigned to the variable , its value doesnt change for the entire program
+  //it can be initiated and assigned value later
+  final num2;
+  num2 = num * 2;
+
+  print("value of num is $num");
+  print("value of num2 is $num2");
 }
